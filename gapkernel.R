20dbe298100f87d3d0968bdfdac4522cddff3cde
Arno Moonens <x-006@hotmail.com>
2016-12-11 23:49:46 +0100

    Improved gap kernel performance

--- gapkernel.R
+++ gapkernel.R
@@ -1,7 +1,6 @@
 # Gap weighted subsequence kernel
 # By Arno Moonens
 
-## To make things simple, we start with our "own version" of the linear kernel
 gapkernel <- function(lambda, p) {
   gapkernelfunction <- function(x, y) 
   {
@@ -29,6 +28,41 @@ gapkernel <- function(lambda, p) {
         for(j in c(1:(c-1))) {
           B[i+1, j+1] <- A[i+1, j+1] + lambda * B[i, j+1] + lambda * B[i+1, j] + lambda^2 * B[i, j]
           if(substr(x, i, i) == substr(y, j, j)) {
+            A[i + 1, j + 1] <- lambda ^ 2 * B[i, j]
+            k[l] <- k[l] + A[i + 1, j + 1]
+          }
+        }
+      }
+    }
+    return(k[p])
+  }
+  return(new("kernel",.Data=gapkernelfunction,kpar=list(lambda = lambda, p = p)))
+}
+
+# Faster version
+gapkernel2 <- function(lambda, p) {
+  gapkernelfunction <- function(x, y) 
+  {
+    x <- strsplit(x, split="")[[1]]
+    y <- strsplit(y, split="")[[1]]
+    r <- length(x)
+    c <- length(y)
+    A <- matrix(0, nrow = r+1, ncol = c+1)
+    B <- matrix(0, nrow = r+1, ncol = c+1)
+    for(i in c(1:r)) {
+      for(j in c(1:c)) {
+        if(x[i] == y[j]) {
+          A[i+1, j+1] <- lambda^2
+        }
+      }
+    }
+    k <- c(1:p)
+    for(l in c(2:p)) {
+      k[l] <- 0
+      for(i in c(1:(r-1))) {
+        for(j in c(1:(c-1))) {
+          B[i+1, j+1] <- A[i+1, j+1] + lambda * B[i, j+1] + lambda * B[i+1, j] + lambda^2 * B[i, j]
+          if(x[i] == y[j]) {
             A[i + 1, j + 1] <- lambda ^ 2 * B[i, j]
             k[l] <- k[l] + A[i + 1, j + 1]
           }
