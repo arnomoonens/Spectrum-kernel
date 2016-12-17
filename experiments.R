a74ebc36a658c0c7d4d2aaacdba1571ed144d557
Arno Moonens <x-006@hotmail.com>
2016-12-17 21:51:45 +0100

    Splitted data into training and test set and added test error computation

--- experiments.R
+++ experiments.R
@@ -2,9 +2,10 @@ library(kernlab)
 library(tm)
 source("gapkernel.R")
 
+set.seed(6)
 data <- read.csv("data.csv", stringsAsFactors = FALSE)
 data <- data[-1]  # Remove first column (indices)
-data <- data[sample(nrow(data), round(0.1 * nrow(data))),]
+data <- data[sample(nrow(data), round(0.15 * nrow(data))),]
 data <- subset(data, target<2)  # Only use three different target values
 
 docs <- Corpus(VectorSource(data$text))
@@ -16,18 +17,14 @@ data$text <- substr(data$text, 1, 750)  # Limit number of characters per text
 
 data$target <- factor(data$target)
 
-# Total number of training examples
+(N <- nrow(data))
+train.indices <- sample(N,round(2*N/3))
+
+# Total number of data points
 length(data$target)
 
 # Number of training examples per topic
 table(data$target)
-
-(N <- dim(data)[1])
-
-# Shuffle the data
-set.seed(6)
-data <- data[sample(1:N, N),]
-
 
 #k <- stringdot("spectrum", length=5, normalized=T)
 # k <- makeCppKernel(0.5, 5)
@@ -54,27 +51,28 @@ data <- data[sample(1:N, N),]
 
 calculate.error  <- function(predictions, true.values) {sum(predictions != true.values) / NROW(true.values)}
 
+data.train <- data[train.indices,]
 # Cross-validation for subsequence length
 k <- 10 # 10-fold cross-validation
 folds.indices <- 1:k
-N <- NROW(data)
-s <- sample(rep(1:k, length=N), N, replace=FALSE) # Say for each data point in which fold it needs to go
+N.train <- NROW(data.train)
+s <- sample(rep(1:k, length=N.train), N.train, replace=FALSE) # Say for each data point in which fold it needs to go
 
 subseq.lengths <- c(2:6)
 mean.errors <- c()
 for (subseq.length in subseq.lengths) {
   kernel <- makeCppKernel(0.5, subseq.length)
-  K <- kernelMatrix(kernel, data$text)
+  K <- kernelMatrix(kernel, data.train$text)
   cv.errors <- c()
   for (idx in folds.indices) {
     train.K <- K[s != idx,s != idx]
-    train.y <- data[s != idx,]$target
+    train.y <- data.train[s != idx,]$target
     
     model <- ksvm(train.K, train.y, type="C-svc", scaled=c(), kernel="matrix")
     
     test.K <- K[s == idx,s != idx]
     test.K <- test.K[, SVindex(model), drop=FALSE]
-    test.y <- data[s == idx,]$target
+    test.y <- data.train[s == idx,]$target
     
     preds <- predict(model, as.kernelMatrix(test.K))
     error.cv <- calculate.error(preds, test.y)
@@ -85,5 +83,12 @@ for (subseq.length in subseq.lengths) {
 }
 min.error.idx <- which.min(mean.errors)
 best.subseq.length <- subseq.lengths[[min.error.idx]]
+cat("Best subsequence length =", best.subseq.length)
+data.test <- data[-train.indices,]
 
+kernel = makeCppKernel(0.5, best.subseq.length)
+model <- ksvm(data.train$text, data.train$target, type="C-svc", scaled=c(), kernel=kernel)
 
+predictions <- predict(model, data.test$text)
+error.test <- calculate.error(predictions, data.test$target)
+cat("Error on test set:", error.test)
