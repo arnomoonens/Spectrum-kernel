213790327a6cf30bcff30d15e361244b6db5071d
henrik Jilke <henrik.jilke@web.de>
2016-12-19 15:55:00 +0100

    update

--- clusteringPart2.R
+++ clusteringPart2.R
@@ -3,12 +3,12 @@
 library(kernlab)
 library(ggplot2)
 require(plot3D)
-soucre("clustering.R")
+
 set.seed(6)
 
 findBestK <- function(K, limit,name, kReal){
-  ss <-vector(length= limit)
-  for( i in 1: limit) ss[i] <- getClusters(K,i)
+  ss <-vector(length = limit)
+  for( i in seq(2,limit+1)) {ss[i-1] <- sum(withinss(kkmeans(K, centers = i, nstart = 10)))}
   k = which.min(ss)
   jpeg('name')
   plot(seq(1,limit), ss, 
@@ -18,19 +18,30 @@ findBestK <- function(K, limit,name, kReal){
 }
 
 N = 30
+
 reuters <- read.table("reuters.txt.gz", header=T)
-kReal = 10
-topics <- sample(unique(reuters[1,]),k)
-
-reuters <- reuters[topics,]
-reuters <- reuters[sample(1:nrow(reuters),N),]
-
 reuters$Content <- as.character(reuters$Content)  
-reuters$Topic <- factor(reuters$Topic)            
+reuters$Topic <- factor(reuters$Topic)
 
-table(reuters$Topic)
+cat = 15
+topics <- factor( sample(unique(reuters$Topic),cat) )
+reuters <- reuters[ sapply(reuters[,1], function (x) x %in% topics) , ]
+reuters <- reuters[sample(1:nrow(reuters),N),]
+kReal = length(unique(reuters[,1]))
+
+set.seed(20)
+
+limit = 10
+k <- stringdot("exponential",length= 6, lambda = 2 )
+K <- kernelMatrix(k, reuters$Content)
+findBestK(K, limit,"EXPONENTIAL",kReal ) 
+
+k <- stringdot("CONSTANT")
+K <- kernelMatrix(k, reuters$Content) 
+findBestK(K,limit,"CONSTANT",kReal ) 
 
 k <- new("kernel", .Data=coSequenceKernelCPP, kpar=list())
 K <- kernelMatrix(k ,reuters$Content)
-findBestK(K,50,"CONNECT",kReal ) 
+findBestK(K,limit,"CONNECT",kReal ) 
+
 
